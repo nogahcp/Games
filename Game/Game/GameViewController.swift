aba8220419796452bd898f5da6a34d7a3ad7a297
nogah <nogah@cherrypick-consulting.com>
2019-10-24 18:03:51 +0300

    added .gitignore file

---
+++ Game/Game/GameViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Game
+//
+//  Created by Nogah Melamed Cohen on 24/10/2019.
+//  Copyright © 2019 Nogah Melamed Cohen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
